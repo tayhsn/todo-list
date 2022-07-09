7e1ac1a81f3d000127beb4055e8f44048f4411ef
Tayanne <tayh.snovaes@gmail.com>
2022-07-09 19:44:03 -0300

    [fix] border NoTask

--- src/components/NoTask.tsx
+++ src/components/NoTask.tsx
@@ -2,7 +2,7 @@ import { ClipboardText } from 'phosphor-react';
 
 export const NoTask = () => {
    return (
-      <div className='flex flex-col items-center border-t border-gray-100 rounded-t-lg px-6 py-16 text-gray-300'>
+      <div className='flex flex-col items-center border-t border-gray-400 rounded-t-lg px-6 py-16 text-gray-300'>
          <ClipboardText size={56} color='gray' />
          <strong>Você ainda não tem tarefas cadastradas</strong>
          <p>Crie tarefas e organize seus itens a fazer</p>
