75b5d3a289e97e13751ccaeb341111b89209bd71
Tayanne <tayh.snovaes@gmail.com>
2022-07-09 18:58:18 -0300

    done NoTask component

---
+++ src/components/NoTask.tsx
@@ -0,0 +1,11 @@
+import { ClipboardText } from 'phosphor-react';
+
+export const NoTask = () => {
+   return (
+      <div className='flex flex-col items-center border-t border-gray-100 rounded-t-lg px-6 py-16 text-gray-300'>
+         <ClipboardText size={56} color='gray' />
+         <strong>Você ainda não tem tarefas cadastradas</strong>
+         <p>Crie tarefas e organize seus itens a fazer</p>
+      </div>
+   );
+};
