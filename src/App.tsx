c5e39645fc1b191bd183816f273c79c71876565e
Tayanne <tayh.snovaes@gmail.com>
2022-07-09 17:46:55 -0300

    done App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
+import { Main } from './components/Main';
 
 function App() {
-
-  return (
-    <h1>Hello</h1>
-  )
+   return (
+      <>
+         <Header />
+         <Main />
+      </>
+   );
 }
 
-export default App
+export default App;
